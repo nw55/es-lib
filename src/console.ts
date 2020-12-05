c2e5e1bf729c0df49d8dabf79ece2b2180e86c17
Niklas Werner <Niklas-Werner@outlook.com>
2020-12-05 16:44:48 +0100

    add LogFilter support for logErrors and logDetails options of ConsoleLogMessageWriter

--- src/console.ts
+++ src/console.ts
@@ -1,4 +1,6 @@
 import { LogMessage } from './common';
+import { LogFilter } from './filter';
+import { LogLevel } from './log-level';
 import { LogMessageWriter } from './message-writer';
 
 type DetailsAndErrorParams =
@@ -6,18 +8,18 @@ type DetailsAndErrorParams =
     | [message: string, error: Error, details?: unknown];
 
 type ConsoleLogMessageWriterOptions = Readonly<{
-    logErrors?: true;
-    logDetails: true;
+    logErrors?: true | LogFilter;
+    logDetails: true | LogFilter;
     writer: (...params: DetailsAndErrorParams) => void;
     errorWriter?: (...params: DetailsAndErrorParams) => void;
 } | {
-    logErrors?: true;
+    logErrors?: true | LogFilter;
     logDetails?: false;
     writer: (message: string, error?: Error) => void;
     errorWriter?: (message: string, error?: Error) => void;
 } | {
     logErrors: false;
-    logDetails: true;
+    logDetails: true | LogFilter;
     writer: (message: string, details?: unknown) => void;
     errorWriter?: (message: string, details?: unknown) => void;
 } | {
@@ -34,6 +36,19 @@ interface Console {
 
 type UnsafeWriter = (text: string, ...args: unknown[]) => void;
 
+const logNothingFilter: LogFilter = {
+    shouldLog: () => false,
+    shouldLogMessage: () => false
+};
+
+function getLogFilter(logFilter: LogFilter | boolean) {
+    if (logFilter === true)
+        return LogLevel.All;
+    if (logFilter === false)
+        return logNothingFilter;
+    return logFilter;
+}
+
 export class ConsoleLogMessageWriter implements LogMessageWriter {
     static createDefault(console: Console, details: 'none' | 'errors' | 'details' | 'all' = 'errors') {
         return new ConsoleLogMessageWriter({
@@ -46,26 +61,26 @@ export class ConsoleLogMessageWriter implements LogMessageWriter {
 
     private _writer: UnsafeWriter;
     private _errorWriter: UnsafeWriter;
-    private _logErrors: boolean;
-    private _logDetails: boolean;
+    private _logErrors: LogFilter;
+    private _logDetails: LogFilter;
 
     constructor(options: ConsoleLogMessageWriterOptions) {
         this._writer = options.writer as UnsafeWriter;
         this._errorWriter = (options.errorWriter ?? options.writer) as UnsafeWriter;
-        this._logErrors = options.logErrors ?? true;
-        this._logDetails = options.logDetails ?? false;
+        this._logErrors = getLogFilter(options.logErrors ?? true);
+        this._logDetails = getLogFilter(options.logDetails ?? false);
     }
 
     writeMessage(text: string, message: LogMessage) {
         const writer = message.level.isError ? this._errorWriter : this._writer;
 
-        if (this._logErrors && message.error !== undefined) {
-            if (this._logDetails && message.details !== undefined)
+        if (this._logErrors.shouldLogMessage(message) && message.error !== undefined) {
+            if (this._logDetails.shouldLogMessage(message) && message.details !== undefined)
                 writer(text, message.error, message.details);
             else
                 writer(text, message.error);
         }
-        else if (this._logDetails && message.details !== undefined) {
+        else if (this._logDetails.shouldLogMessage(message) && message.details !== undefined) {
             writer(text, message.details);
         }
         else {
