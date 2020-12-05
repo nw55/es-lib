c6e3c6163fec56a6ca770079e7efeb0f16a3bab8
Niklas Werner <Niklas-Werner@outlook.com>
2020-12-05 20:16:52 +0100

    improved console logging

--- src/logging.ts
+++ src/logging.ts
@@ -1,41 +1,51 @@
-import { ConsoleLogMessageWriter, DefaultLogWriter, LogFilter, logFormat, LogFormat, LogMessageWriter, TransformedLogMessageWriter } from '@nw55/logging';
+import { ConsoleLogMessageWriter, DefaultLogWriter, Log, LogFilter, logFormat, LogFormat, LogMessageWriter, TransformedLogMessageWriter } from '@nw55/logging';
 
-export namespace ConsoleLogWriter {
-    type ColorStyler<K extends string> = {
-        [P in K]: ColorStyler<K> & ((text: string) => string);
-    };
+type ColorStyler<K extends string> = {
+    [P in K]: ColorStyler<K> & ((text: string) => string);
+};
 
-    type DefaultColorStyler = ColorStyler<'gray' | 'cyanBright' | 'whiteBright' | 'greenBright' | 'yellowBright' | 'redBright' | 'red' | 'bgWhiteBright'>;
+type DefaultColorStyler = ColorStyler<'gray' | 'cyanBright' | 'whiteBright' | 'greenBright' | 'yellowBright' | 'redBright' | 'red' | 'bgWhiteBright'>;
 
-    function useColorStyler(baseWriter: LogMessageWriter, colorStyler?: DefaultColorStyler) {
-        if (colorStyler === undefined)
-            return baseWriter;
+function useColorStyler(baseWriter: LogMessageWriter, colorStyler?: DefaultColorStyler) {
+    if (colorStyler === undefined)
+        return baseWriter;
 
-        return TransformedLogMessageWriter.byLevel(baseWriter, {
-            all: text => colorStyler.gray(text),
-            debug: text => colorStyler.whiteBright(text),
-            info: text => colorStyler.cyanBright(text),
-            notice: text => colorStyler.greenBright(text),
-            warn: text => colorStyler.yellowBright(text),
-            error: text => colorStyler.greenBright(text),
-            critical: text => colorStyler.bgWhiteBright.red(text)
-        });
-    }
+    return TransformedLogMessageWriter.byLevel(baseWriter, {
+        all: text => colorStyler.gray(text),
+        debug: text => colorStyler.whiteBright(text),
+        info: text => colorStyler.cyanBright(text),
+        notice: text => colorStyler.greenBright(text),
+        warn: text => colorStyler.yellowBright(text),
+        error: text => colorStyler.greenBright(text),
+        critical: text => colorStyler.bgWhiteBright.red(text)
+    });
+}
 
-    export const defaultFormat = logFormat`${'datetime'} ${'level'} [${'source'}]${logFormat.codeFormat(' %')}: ${'message'}`;
+export const defaultConsoleLogFormat = logFormat`${'datetime'} ${'level'} [${'source'}]${logFormat.codeFormat(' %')}: ${'message'}`;
 
-    interface Options {
-        readonly filter?: LogFilter;
-        readonly format?: LogFormat;
-        readonly details?: 'none' | 'errors' | 'details' | 'all';
-        readonly colorStyler?: DefaultColorStyler;
-    }
+export interface ConsoleLogWriterOptions {
+    readonly filter?: LogFilter;
+    readonly format?: LogFormat;
+    readonly logErrors?: boolean | LogFilter;
+    readonly logDetails?: boolean | LogFilter;
+    readonly colorStyler?: DefaultColorStyler;
+}
 
-    export function create(options: Options) {
-        return new DefaultLogWriter({
-            filter: options.filter,
-            format: options.format ?? defaultFormat,
-            messageWriter: useColorStyler(ConsoleLogMessageWriter.createDefault(console, options.details), options.colorStyler)
-        });
-    }
+export function createConsoleLogWriter(options: ConsoleLogWriterOptions) {
+    const messageWriter = new ConsoleLogMessageWriter({
+        writer: console.log.bind(console), // eslint-disable-line no-console
+        errorWriter: console.error.bind(console), // eslint-disable-line no-console
+        logDetails: options.logDetails,
+        logErrors: options.logErrors
+    });
+
+    return new DefaultLogWriter({
+        filter: options.filter,
+        format: options.format ?? defaultConsoleLogFormat,
+        messageWriter: useColorStyler(messageWriter, options.colorStyler)
+    });
+}
+
+export function useDefaultConsoleLogging(filter?: LogFilter) {
+    Log.addGlobalLogWriter(createConsoleLogWriter({ filter }));
 }
